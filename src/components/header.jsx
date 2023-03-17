4207aad0600929e06917d7ea1ece9a7c0cd923db
buglib <buglib@foxmail.com>
2023-03-17 17:52:55 +0800

    尝试使用taro的AtInput和AtButtion组件替换Header组件中原有的input和button, 然而AtInput在taro 3.6.x版本上有focus的问题(https://github.com/NervJS/taro/issues/13316)

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,28 @@
 import React from "react"
 import { Component } from "react"
+import { AtInput, AtButton } from "taro-ui"
+
+import "taro-ui/dist/style/components/input.scss"
+import "taro-ui/dist/style/components/button.scss"
+// import "taro-ui/dist/style/components/loading.scss"
 
 export default class Header extends Component {
   render() {
+    const focus = false
     return (
       <div className="todolist-header">
-        <input id="input-task" type="text" placeholder="请输入待办任务" />
-        <button onClick={() => this.handleClick()}>添加</button>
+        <input
+          id="input-task"
+          type="text"
+          placeholder="请输入待办任务"
+        />
+
+        <AtButton
+          type="primary"
+          size="small"
+          onClick={() => this.handleClick}
+        >添加
+        </AtButton>
       </div>
     )
   }
